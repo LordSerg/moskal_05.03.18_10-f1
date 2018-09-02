aa06428d312d0c5694ae53d67ba67c5a200321d3
Moskalenko Sergey <oknelaksoms@gmail.com>
2018-09-02 13:43:46 +0300

    рисование полигона с помощью мыши

---
+++ moskal_05.03.18_10-f(1)/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace moskal_05._03._18_10_f_1_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Периметр=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 46);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "...";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(423, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 52);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Перемалювати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(42, 26);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "5";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "n=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 471);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Побудова n-кутника за допомогою миші";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ moskal_05.03.18_10-f(1)/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace moskal_05._03._18_10_f_1_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int P=0;//сума довжин сторін
+        int a;//лічильник намальованих крапок фігури
+        int n;//всього крапок в фігурі
+        //х і у - масиви точок
+        int[] x;
+        int[] y;
+        double[] P1;//масив довжини кожної прямої
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            n = Convert.ToInt32(textBox1.Text);
+            a = -1;
+            x = new int[n];
+            y = new int[n];
+            this.BackColor = Color.White;
+            P1 = new double[n+1];
+            
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            a++;
+            if (a < n&&a>-1)
+            {
+                timer1.Enabled = true;
+                timer1.Interval = 1;
+                x[a] = MousePosition.X - this.Location.X;
+                y[a] = MousePosition.Y - this.Location.Y;
+                Pen pen = new Pen(Color.Blue, 2);
+                Graphics g = CreateGraphics();
+                g.DrawEllipse(pen, x[a] - 5, y[a] - 5, 10, 10);
+            }
+            else
+            {
+                timer1.Enabled = false;
+            }
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            
+            if (Convert.ToInt32(textBox1.Text) < 3)
+            {
+                MessageBox.Show("Замало точок для побудови фігури");
+            }
+            else
+            {
+                timer1.Enabled = false;
+                a = -1;
+                n = Convert.ToInt32(textBox1.Text);
+                x = new int[n];
+                y = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    x[i] = 0;
+                    y[i] = 0;
+                }
+                this.Refresh();
+
+                P1 = new double[n + 1];
+                P = 0;
+                label3.Text = P.ToString();
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            System.Drawing.Font font = new System.Drawing.Font(new FontFamily("Arial"),12);
+            P = 0;
+            Pen pen = new Pen(Color.Blue, 2);
+            Pen p = new Pen(Color.Black,3);
+            Pen p2 = new Pen(Color.White, 3);
+            Graphics g = CreateGraphics();
+            for (int j = 0; j < n; j++)
+            {
+                if(a==j+1&&a!=0&&a!=n-1)
+                {
+                    g.DrawLine(p, x[j], y[j], x[j + 1], y[j + 1]);
+                    P1[j] = Math.Sqrt(Math.Abs(x[j] - x[j + 1]) * Math.Abs(x[j] - x[j + 1]) + Math.Abs(y[j] - y[j + 1]) * Math.Abs(y[j] - y[j + 1]));
+                    g.DrawEllipse(pen, x[j] - 5, y[j] - 5, 10, 10);
+                }
+                if (a == n - 1)
+                {
+                    g.DrawLine(p, x[a - 1], y[a - 1], x[a], y[a]);
+                    g.DrawLine(p, x[0], y[0], x[a], y[a]);
+                    P1[a - 1] = Math.Sqrt(Math.Abs(x[a - 2] - x[a - 1]) * Math.Abs(x[a - 2] - x[a - 1]) + Math.Abs(y[a - 2] - y[a - 1]) * Math.Abs(y[a - 2] - y[a - 1]));
+                    P1[a] = Math.Sqrt(Math.Abs(x[0] - x[a - 1]) * Math.Abs(x[0] - x[a - 1]) + Math.Abs(y[0] - y[a - 1]) * Math.Abs(y[0] - y[a - 1]));
+                    g.DrawEllipse(pen, x[j] - 5, y[j] - 5, 10, 10);
+                    g.DrawString((j+1).ToString(), font, Brushes.Blue, x[j] + 10, y[j] + 10);
+                    Point[] point;
+                    point = new Point[n];
+                    for (int i = 0; i < n; i++)
+                    {
+                        point[i].X = x[i];
+                        point[i].Y = y[i];
+                    }
+                    g.FillPolygon(Brushes.Red, point);
+                    
+                }
+            }
+            for (int i = 0; i < a+1; i++)
+            {
+                g.DrawEllipse(pen, x[i] - 5, y[i] - 5, 10, 10);
+                g.DrawString((i+1).ToString(),font,Brushes.Blue,x[i]+10,y[i]+10);
+                
+            }
+            for(int i=0;i<n+1;i++)
+            {
+                P += Convert.ToInt32(P1[i]);
+            }
+            label3.Text = P.ToString();
+            if(a==n-1)
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
